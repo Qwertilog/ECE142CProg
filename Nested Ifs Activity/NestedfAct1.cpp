090a7552242ff8c9f1795f3aed014012b6406bcf
Qwertilog <jethro.caingat@gmail.com>
2023-05-14 03:02:06 +0800

    Test

---
+++ Nested Ifs Activity/NestedfAct1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int grade;
+    char remark;
+
+    cout << "This program outputs the equivalent\n"
+         << "grade of the user.\n"
+         << "-----------------------------------\n"
+         << "Please enter your grade: ";
+    cin >> grade;
+
+    if (grade >= 50 && grade <=100)
+    {
+        if (grade <= 60)
+            remark = 'F';
+        else if (grade <= 70)
+            remark = 'D';
+        else if (grade <= 80)
+            remark = 'C';
+        else if (grade <= 90)
+            remark = 'B';
+        else
+            remark = 'A';
+
+        cout << "\nYour equivalent grade is: "
+             << remark << endl;
+
+    }
+    else
+        cout << "\nYour grade is out of the numerical range (50-100)\n";
+
+    return 0;
+}
---
+++ Nested Ifs Activity/NestedfSupp1.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+    int flv,
+        sze,
+        menu;
+    char snr,
+         ret;
+    string size,
+           flavor,
+           size_menu,
+           flavor_menu;
+    double price;
+
+    size_menu = "\nPlease choose the size of your pizza.\n"
+                "      Size              Price\n"
+                "1. Small          Starts at PHP65.00\n"
+                "2. Regular        Starts at PHP95.00\n"
+                "3. Large          Starts at PHP130.00\n"
+                "4. Extra Large    Starts at PHP185.00\n\n"
+                "Enter the number you want to choose: ";
+
+    cout << "This program computes the user's total bill based on their\n"
+         << "choice of pizza.\n"
+         << "----------------------------------------------------------\n\n";
+
+    cout << "**********************************************************\n"
+         << "*      Welcome to Jethro's Pizzeria Ordering Kiosk!      *\n"
+         << "**********************************************************\n\n"
+         << "To start ordering, please choose a menu.\n"
+         << "1. Size\n"
+         << "2. Flavor\n"
+         << "Enter the number you want to choose: ";
+    cin >> menu;
+
+    switch (menu)
+    {
+        case 1:
+            cout << size_menu;
+            cin >> sze;
+
+            switch (sze)
+            {
+                case 1:
+                    price += 65.00;
+                    size = "Small Pizza......................PHP65.00";
+                    break;
+                case 2:
+                    price += 95.00;
+                    size = "Regular Pizza....................PHP95.00";
+                    break;
+                case 3:
+                    price += 130.00;
+                    size = "Large Pizza......................PHP130.00";
+                    break;
+                case 4:
+                    price += 185.00;
+                    size = "Extra Large Pizza................PHP185.00";
+                    break;
+                default:
+                    cout << "\nPlease choose a valid number.\n";
+                    return 0;
+            }
+
+            cout << "\n\n-------------------------------------------------"
+                 << "\nYour current order is:\n"
+                 << size
+                 << "\n\nTotal price: PHP"
+                 << setprecision(2) << fixed
+                 << price << endl
+                 << "-------------------------------------------------\n";
+            break;
+
+        case 2:
+            cout << "\nPlease choose the flavor of your pizza:\n"
+                 << "           Flavor                  Price\n"
+                 << "1. Regular Ham and Cheese      Add PHP0.00\n"
+                 << "2. Pepperoni                   Add PHP25.00\n"
+                 << "3. Hawaiian                    Add PHP30.00\n"
+                 << "4. Bacon and Mushroom BBQ      Add PHP55.00\n\n"
+                 << "Enter the number you want to choose: ";
+            cin >> flv;
+
+            switch (flv)
+            {
+                case 1:
+                    flavor = "Regular Ham and Cheese Flavor....PHP0.00";
+                    break;
+                case 2:
+                    price += 25.00;
+                    flavor = "Pepperoni Flavor.................PHP25.00";
+                    break;
+                case 3:
+                    price += 30.00;
+                    flavor = "Hawaiian Flavor..................PHP30.00";
+                    break;
+                case 4:
+                    price += 55.00;
+                    flavor = "Bacon and Mushroom BBQ Flavor....PHP55.00";
+                    break;
+                default:
+                    cout << "\nPlease choose a valid number.\n";
+                    return 0;
+            }
+            cout << "\n\n-------------------------------------------------"
+                 << "\nYour current order is:"
+                 << "\n" << size << endl
+                 << flavor
+                 << "\n\nTotal price: PHP"
+                 << setprecision(2) << fixed
+                 << price << endl
+                 << "-------------------------------------------------\n";
+            break;
+    }
+
+    cout << "Do you want to change your order?\n"
+         << "Enter (y/Y) if YES or (n/N) if NO: ";
+    cin >> ret;
+
+    cout << "\nDo you have a Senior Citizen Card?\n"
+         << "Enter (y/Y) if YES or (n/N) if NO: ";
+    cin >> snr;
+
+    if (snr == 'y' || snr == 'Y')
+        {
+            cout << "\n================================================="
+                 << "\nYour final pizza order is:\n"
+                 << "    " << size
+                 << "\n    " << flavor
+                 << "\n\nTotal Price:"
+                 << "\n    PHP"
+                 << setprecision(2) << fixed
+                 << price << endl;
+
+                 price -= (price * 0.10);
+
+            cout << "\nDeductions:"
+                 << "\n    Senior Citizen Discount (10%)"
+                 << "\n\nDiscounted Price:"
+                 << "\n    PHP"
+                 << setprecision(2) << fixed
+                 << price << endl
+                 << "\nPlease proceed to the counter for payment.\n"
+                 << "=================================================\n";
+        }
+    else if (snr == 'n' || snr == 'N')
+        cout << "\n================================================="
+             << "\nYour final pizza order is:\n"
+             << "    " << size
+             << "\n    " << flavor
+             << "\n\nTotal Price:"
+             << "\n    PHP"
+             << setprecision(2) << fixed
+             << price << endl
+             << "\nPlease proceed to the counter for payment.\n"
+             << "=================================================\n";
+    else
+        cout << "\nPlease enter a valid character.\n";
+
+    /*cout << "\n-----------------------------------------\n"
+         << "Do you want to change your order?"
+         << "Enter (y/Y) if YES or (n/N) if NO: ";
+    cin >> ret;*/
+
+    return 0;
+}
